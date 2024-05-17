8073adc7e4affe2ade96c18e9a03d6702eb8ae08
orxannitro5 <orxannahmedovv@gmail.com>
2024-05-17 06:49:15 +0400

    dsadas

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Layiheler from "./layiheler-section";
+import Error from "./error";
+function App() {
+  return (
+   <>
+   {/* <Layiheler /> */}
+   <Error  />
+   </>
+  );
+}
+
+export default App;
